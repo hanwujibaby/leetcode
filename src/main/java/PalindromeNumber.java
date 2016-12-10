9edd945ebbc42b31f0ed5d98adc18a069668c531
韦兴 <weixing@alibaba-inc.com>
2016-12-11 00:13:36 +0800

    Palindrome Number

---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * Created by wei4liverpool on 12/9/16.
+ */
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindarome(121));
+        
+    }
+
+
+    public static boolean isPalindarome(int n){
+        if(n<0){
+            return false;
+        }else if (n==0){
+            return true;
+        }else {
+            int x=n%10;
+            int k=n/10;
+            int sum=0;
+            sum+=x;
+
+            while(k>sum){
+                x=k%10;
+                k=k/10;
+                sum=sum*10+x;
+            }
+            if(k==sum){
+                return true;
+            }else if (k<sum){
+                if(sum==(k*10+sum%10)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        return false;
+    }
+
+
+}
