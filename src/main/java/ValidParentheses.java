180859349348739f86bddf0e76859212ef88a773
韦兴 <weixing@alibaba-inc.com>
2016-12-20 11:51:18 +0800

    Valid Parentheses solved:增加验证testcas: ()]

--- src/main/java/ValidParentheses.java
+++ src/main/java/ValidParentheses.java
@@ -4,51 +4,70 @@ import java.util.Stack;
 /**
  * Created by wei4liverpool on 12/12/16.
  *
- * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
- The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
-
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the
+ * input string is valid.
+ * 
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]"
+ * are not.
+ * 
  */
 
 public class ValidParentheses {
 
- public static void main(String[] args) {
-  System.out.println(isValidParentheses("()[]]"));
+  public static void main(String[] args) {
+    System.out.println(isValidParentheses("()"));
 
- }
+  }
 
 
- public  static boolean isValidParentheses(String text){
-  HashMap<Character,Character> map=new HashMap<Character, Character>();
-  map.put('(',')');
-  map.put('{','}');
-  map.put('[',']');
+  public static boolean isValidParentheses(String text) {
+    HashMap<Character, Character> map = new HashMap<Character, Character>();
+    map.put('(', ')');
+    map.put('{', '}');
+    map.put('[', ']');
 
-  char[] chars=text.toCharArray();
-  Stack<Character> stack=new Stack<Character>();
+    char[] chars = text.toCharArray();
 
-  for(int i=0;i<chars.length;i++){
-   if(map.containsKey(chars[i])){
-    stack.push(chars[i]);
-   }else if(map.containsValue(chars[i])){
-    if(stack.isEmpty()==false && !isPair(stack.peek(),chars[i])){
-     return false;
+    if (chars.length <= 1) {
+      return false;
     }
-   }else{
-    return false;
-   }
+
+    Stack<Character> stack = new Stack<Character>();
+
+    for (int i = 0; i < chars.length; i++) {
+
+      if (map.containsKey(chars[i])) {
+
+        stack.push(chars[i]);
+
+      } else if (map.containsValue(chars[i])) {
+
+        if (!stack.isEmpty()) {
+              if(!isPair(stack.pop(),chars[i])){
+                  return false;
+              }
+          }else{
+            return false;
+        }
+
+      } else {
+
+        return false;
+
+      }
+
+    }
+
+    if (stack.empty() == false) {
+      return false;
+    } else {
+      return true;
+    }
 
   }
-  if(stack.empty()==false){
-   return false;
-  }else{
-   return true;
+
+  public static boolean isPair(char c1, char c2) {
+    return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}');
   }
-
- }
-
- public static boolean isPair(char c1,char c2){
-  return (c1=='('&&c2==')')||(c1=='['&&c2==']')||(c1=='{'&&c2=='}');
- }
 
 }
