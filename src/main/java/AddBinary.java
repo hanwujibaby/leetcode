526c9fffb44381d81c207bc25a5d3680b45266a2
韦兴 <weixing@alibaba-inc.com>
2016-12-23 15:28:22 +0800

    init Add Binary

---
+++ src/main/java/AddBinary.java
@@ -0,0 +1,20 @@
+/**
+ * Created by wei4liverpool on 12/23/16.
+ * Given two binary strings, return their sum (also a binary string).
+
+ For example,
+ a = "11"
+ b = "1"
+ Return "100".
+ *
+ */
+public class AddBinary {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String addBinary(String a, String b) {
+        return null;
+    }
+}
