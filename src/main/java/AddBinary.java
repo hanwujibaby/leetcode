8a20228b19679237ea2d7825af54fd5237b81194
韦兴 <weixing@alibaba-inc.com>
2016-12-27 19:19:25 +0800

    AddBinary:和Plus one类似，采用进位方式，主要考虑3种情况：在进位上考虑2,3,0,1

--- src/main/java/AddBinary.java
+++ src/main/java/AddBinary.java
@@ -10,7 +10,10 @@
 public class AddBinary {
 
     public static void main(String[] args) {
-        System.out.println(addBinary("11","1"));
+        int a=1;
+        //System.out.println(Integer.toString(a).charAt(0));
+        String s=addBinary("11","1");
+        System.out.println(s);
 
 
     }
@@ -29,31 +32,49 @@ public class AddBinary {
             return a;
         }
 
-        String lStr=a.length()>b.length()?a:b;
+        String lStr=a.length()>=b.length()?a:b;
         String sStr=a.length()<b.length()?a:b;
+
 
         char[] lChars=lStr.toCharArray();
         char[] sChars=sStr.toCharArray();
         char[] finalChars= new char[lChars.length+1];
-        int dot=0;
 
-        for(int i=sChars.length-1,j=lChars.length-1;i>=0&&j>=0;i--,j--){
-            if(Character.digit(sChars[i],10)+(Character.digit(lChars[j],10))==2){
+        for(int i=sChars.length-1,j=lChars.length-1;j>=0;i--,j--){
+
+            int result=i<0?(Character.digit(lChars[j],10)): Character.digit(sChars[i],10)+Character.digit(lChars[j],10);
+            if(result==2){
                 if(j!=0){
+                    lChars[j-1]=Integer.toString(Character.digit(lChars[j-1],10)+1).charAt(0);
                     lChars[j]='0';
+
                 }else{
                     System.arraycopy(lChars,0,finalChars,1,lChars.length);
-                    lChars[0]='1';
-                    lChars[i+1]='0';
+                    finalChars[0]='1';
+                    finalChars[j+1]='0';
+                }
+            }else if(result==3){
+                if(j!=0){
+                    lChars[j-1]=Integer.toString(Character.digit(lChars[j-1],10)+1).charAt(0);
+                    lChars[j]='1';
+                }else{
+                    System.arraycopy(lChars,0,finalChars,1,lChars.length);
+                    finalChars[0]='1';
+                    finalChars[j+1]='1';
+
                 }
             }else{
-                lChars[i]='1';
-                break;
+                if(i<0){
+                    lChars[j]=Integer.toString(Character.digit(lChars[j],10)).charAt(0);
+
+                }else{
+                    lChars[j]=Integer.toString(Character.digit(lChars[j],10)+Character.digit(sChars[i],10)).charAt(0);
+                }
             }
 
         }
 
-        return String.copyValueOf(finalChars);
+        return finalChars[0]=='1'?String.copyValueOf(finalChars) :String.copyValueOf(lChars);
     }
 
 }
