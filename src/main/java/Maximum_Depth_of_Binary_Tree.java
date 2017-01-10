4a22ec8bf5e7e56965092219826a63efaf5f5a90
韦兴 <weixing@alibaba-inc.com>
2017-01-10 11:12:17 +0800

    Maximum Depth of Bianary Tree

---
+++ src/main/java/Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Created by wei4liverpool on 1/10/17.
+ * Given a binary tree, find its maximum depth.
+
+ The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ 寻找二叉树的最长子节点的深度
+
+ public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Maximum_Depth_of_Binary_Tree {
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public int maxDepth(TreeNode root) {
+        return 0;
+    }
+
+
+    public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode(int x) { val = x; }
+    }
+
+
+
+}
