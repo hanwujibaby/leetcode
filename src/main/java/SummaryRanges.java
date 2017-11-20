6f55cd59029054edf311c3e4181c0f3b17c582dc
wei4lfc <wayleynam@gmail.com>
2017-11-20 16:47:01 +0800

    Summary Ranges:hash+list即可解决

---
+++ src/main/java/SummaryRanges.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+/**
+ *
+ * Given a sorted integer array without duplicates, return the summary of its ranges.
+ * Example 1:
+ Input: [0,1,2,4,5,7]
+ Output: ["0->2","4->5","7"]
+
+
+
+ Example 2:
+ Input: [0,2,3,4,6,8,9]
+ Output: ["0","2->4","6","8->9"]
+
+
+ 和之前的算法类似，hash+list即可解决。
+ * Created by weixing on 17/11/20.
+ */
+public class SummaryRanges {
+
+  public static void main(String[] args) {
+
+  }
+
+
+  public List<String> summaryRanges(int[] nums) {
+    return null;
+  }
+
+}
