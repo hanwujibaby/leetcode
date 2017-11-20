c9a05a5cbb840df59b4cd0c2feecbf559ab868ba
wei4lfc <wayleynam@gmail.com>
2017-11-20 16:48:42 +0800

    Given an integer, write a function to determine if it is a power of two.

---
+++ src/main/java/PowerofTwo.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * Given an integer, write a function to determine if it is a power of two.
+
+
+ * Created by weixing on 17/11/20.
+ */
+public class PowerofTwo {
+
+  public static void main(String[] args) {
+
+  }
+
+
+  public boolean isPowerOfTwo(int n) {
+    return false;
+
+  }
+
+
+}
