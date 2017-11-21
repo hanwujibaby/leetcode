5d3d67b71411604e9d7bc6bd97c464e750927bd1
wei4lfc <wayleynam@gmail.com>
2017-11-21 11:10:58 +0800

    Add Digits:使用数学办法

---
+++ src/main/java/AddDigits.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+ For example:
+
+ Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ * Created by weixing on 17/11/21.
+ */
+public class AddDigits {
+  public static void main(String[] args) {
+
+  }
+
+
+  public int addDigits(int num) {
+    return (num-1)%9+1;
+  }
+
+
+
+
+}
