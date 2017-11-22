10dad80de14976eef011ebc0e0eca7d4b0386e19
wei4lfc <wayleynam@gmail.com>
2017-11-22 14:53:28 +0800

    Lowest_Common_Ancestor_of_a_Binary_Tree

---
+++ src/main/java/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,52 @@
+/**
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+ According to the definition of LCA on Wikipedia:
+ “The lowest common ancestor is defined between two nodes v and w as the lowest node in T
+ that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+
+    _______3______
+    /              \
+ ___5__          ___1__
+ /      \        /      \
+ 6      _2       0       8
+ /  \
+ 7   4
+
+
+ *For example, the lowest common ancestor (LCA) of nodes
+ * 5 and 1 is 3.
+ * Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself
+ * according to the LCA definition.
+
+
+ *
+ *
+ * Created by weixing on 17/11/21.
+ */
+public class Lowest_Common_Ancestor_of_a_Binary_Tree {
+
+  public static void main(String[] args) {
+
+  }
+
+
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    return null;
+
+  }
+
+
+  public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+
+
+}
