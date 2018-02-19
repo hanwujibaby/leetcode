e0e6b055f20fd76308d2b06a0ca4e55d1cb8e6d2
wei4lfc <wei4lfc@Wei4lfcs-MacBook-Pro.local>
2018-02-20 02:17:20 +0800

    NumArray：给定一个整型数组，计算某个区间的值，不能用便利，会比较慢

---
+++ src/main/java/NumArray.java
@@ -0,0 +1,40 @@
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ * <p>
+ * Example:
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * <p>
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * Note:
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ * <p>
+ * Created by wei4lfc on 20/02/2018.
+ */
+public class NumArray {
+    public static void main(String[] args) {
+
+    }
+
+
+    private int[] nums;
+
+    public NumArray(int[] nums) {
+        nums = nums;
+    }
+
+    public int sumRange(int i, int j) {
+
+    }
+
+
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray obj = new NumArray(nums);
+     * int param_1 = obj.sumRange(i,j);
+     */
+
+
+}
