dd99396e60460939344eb523e2f7abc053715e18
weixing <weixing4@cmbc.com.cn>
2019-09-30 00:24:54 +0800

    1、利用list和栈来循环保存左右节点 2、利用栈来维护插入顺序

---
+++ src/main/java/BinaryTreeLevelOrderTraversal2.java
@@ -0,0 +1,117 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * <p>
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * <p>
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15  7
+ * <p>
+ * <p>
+ * <p>
+ * 返回其自底向上的层次遍历为：
+ * <p>
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
+
+public class BinaryTreeLevelOrderTraversal2 {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        TreeNode left1 = new TreeNode(9);
+        TreeNode right1 = new TreeNode(20);
+        TreeNode left2 = new TreeNode(15);
+        TreeNode right2 = new TreeNode(7);
+        root.left = left1;
+        root.right = right1;
+        left1.left = left2;
+        right1.right = right2;
+
+
+        List<List<Integer>> resulst = levelOrderBottom(root);
+        String re = "";
+        for (int i = 0; i < resulst.size(); i++) {
+            String s = "";
+            for (int j = 0; j < resulst.get(i).size(); j++) {
+                s += resulst.get(i).get(j) + ";";
+            }
+            re += s + "\n";
+            System.out.println(re);
+        }
+
+    }
+
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> resultList = new LinkedList<>();
+        Stack<List<Integer>> stack = new Stack<List<Integer>>();
+        List<Integer> nodeList = new LinkedList<Integer>();
+        List<TreeNode> leafList = new LinkedList<TreeNode>();
+        if (root != null) {
+            nodeList.add(root.val);
+            stack.push(nodeList);
+            if (root.left != null) {
+                leafList.add(root.left);
+            }
+            if (root.right != null) {
+                leafList.add(root.right);
+            }
+        }
+
+        while (leafList.isEmpty() == false) {
+            nodeList=new LinkedList<Integer>();
+            List<TreeNode> tmpList = new LinkedList<TreeNode>();
+            for (int i = 0; i < leafList.size(); i++) {
+                TreeNode node = leafList.get(i);
+                nodeList.add(node.val);
+                if (node.left != null) {
+                    tmpList.add(node.left);
+                }
+                if (node.right != null) {
+                    tmpList.add(node.right);
+                }
+            }
+
+
+            if (nodeList.isEmpty() == false) {
+                stack.push(nodeList);
+            }
+
+            leafList.clear();
+            for (int i = 0; i < tmpList.size(); i++) {
+                leafList.add(tmpList.get(i));
+            }
+        }
+
+        while (stack.isEmpty() == false) {
+            resultList.add(stack.pop());
+        }
+
+        return resultList;
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
--- src/main/java/ClimbingStairs.java
+++ src/main/java/ClimbingStairs.java
@@ -2,42 +2,38 @@ import java.util.HashMap;
 
 /**
  * Created by wei4liverpool on 12/27/16.
- *
+ * <p>
  * You are climbing a stair case. It takes n steps to reach to the top.
-
- Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
- solusion:递归求解,最后到达n的时候,一定是n-1,或者n-2,得出递归公司f(n)=f(n-1)+f(n-2);但是由于复杂度过高,dp的话,要求递归+缓存;故存上之前的值.
-
- *
- *
+ * <p>
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * <p>
+ * solusion:递归求解,最后到达n的时候,一定是n-1,或者n-2,得出递归公司f(n)=f(n-1)+f(n-2);但是由于复杂度过高,dp的话,要求递归+缓存;故存上之前的值.
  */
 public class ClimbingStairs {
 
-    static HashMap<Integer,Integer> map=new HashMap<Integer, Integer>();
+    static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
     public static void main(String[] args) {
-        long s1=System.currentTimeMillis();
+        long s1 = System.currentTimeMillis();
         System.out.println(climbStairs(30));
-        System.out.println(System.currentTimeMillis()-s1+"ms");
+        System.out.println(System.currentTimeMillis() - s1 + "ms");
 
     }
 
 
-
-
     public static int climbStairs(int n) {
-        if(n<=3){
-            map.put(n,n);
+        if (n <= 3) {
+            map.put(n, n);
             return n;
         }
-        Integer x=map.get(n);
-        if(x!=null){
+        Integer x = map.get(n);
+        if (x != null) {
             return x;
         }
-        int n1= climbStairs(n-1);
-        int n2= climbStairs(n-2);
-        map.put(n,n1+n2);
-        return n1+n2;
+        int n1 = climbStairs(n - 1);
+        int n2 = climbStairs(n - 2);
+        map.put(n, n1 + n2);
+        return n1 + n2;
     }
 
 }
