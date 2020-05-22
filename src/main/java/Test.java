81c3c7c1c8a94eac75a76468b7ce1054b62c983f
weixing <weixing4@cmbc.com.cn>
2020-05-22 14:22:41 +0800

    test3

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("done");
         System.out.println("done1");
+        System.out.println("done2");
     }
 }
